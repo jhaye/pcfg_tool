94a559e65ec26b285427e8f809f409c0e677d53c
Johannes Hayeß <jhaye@mailbox.org>
2022-06-06 17:57:51 +0200

    Add debinaraization routine

---
+++ src/binarized/debinarize.rs
@@ -0,0 +1,58 @@
+use super::node::{Binarized, MarkovizedNode};
+use crate::tree::Tree;
+
+impl<A> Tree<Binarized<A>> {
+    fn debinarize(mut self) -> Tree<A> {
+        if self.is_leaf() {
+            let root = match self.root {
+                Binarized::Bare(a) => a,
+                Binarized::Markovized(_) => panic!("Leaf node cannot be markovized!"),
+            };
+
+            Tree {
+                root,
+                children: vec![],
+            }
+        } else {
+            if let Binarized::Markovized(_) = self.children.iter().last().unwrap().root {
+                let last = self.children.pop().unwrap();
+                self.children.extend(last.children);
+                self.debinarize()
+            } else {
+                let root = match self.root {
+                    Binarized::Bare(a) => a,
+                    Binarized::Markovized(MarkovizedNode {
+                        label: a,
+                        children: _,
+                        ancestors: _,
+                    }) => a,
+                };
+
+                Tree {
+                    root,
+                    children: self.children.drain(..).map(|c| c.debinarize()).collect(),
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::sexp::SExp;
+    use std::str::FromStr;
+
+    #[test]
+    fn debinarize_successful() {
+        let binarized_tree = Tree::from(
+            SExp::from_str("(ROOT (FRAG^<ROOT> (RB (Not)) (FRAG|<NP-TMP,.>^<ROOT> (NP-TMP^<FRAG,ROOT> (DT (this)) (NN (year))) (. (.)))))")
+                .unwrap(),
+        );
+        let binarized_tree = binarized_tree.parse_markovized();
+        assert_eq!(
+            "(ROOT (RB Not) (NP-TMP (DT this) (NN year)) (. .))".to_string(),
+            format!("{}", binarized_tree.debinarize())
+        );
+    }
+}
